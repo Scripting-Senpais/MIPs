4684c85cbbaafbc8add15e798bb15d285847331b
sandyy44 <sandyalaa4@yahoo.com>
2024-05-18 14:57:45 +0300

    Final Changes

--- design1/src/TestBench/MIPS_TB.vhd
+++ design1/src/TestBench/MIPS_TB.vhd
@@ -1,46 +1,22 @@
----------------------------------------------------------------------------------------------------
---
--- Title       : Test Bench for mips
--- Design      : design1
--- Author      : Sandy 3Laa
--- Company     : Dell
---
----------------------------------------------------------------------------------------------------
---
--- File        : $DSN\src\TestBench\mips_TB.vhd
--- Generated   : 5/10/2024, 6:46 PM
--- From        : $DSN\src\mips.vhd
--- By          : Active-HDL Built-in Test Bench Generator ver. 1.2s
---
----------------------------------------------------------------------------------------------------
---
--- Description : Automatically generated Test Bench for mips_tb
---
----------------------------------------------------------------------------------------------------
-
 library ieee;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 
-	-- Add your library and packages declaration here ...
+
 
 entity mips_tb is
 end mips_tb;
 
 architecture TB_ARCHITECTURE of mips_tb is
-	-- Component declaration of the tested unit
+
 	component mips
 	port(
 		clk : in std_logic;
 		reset : in std_logic );
 	end component;
 
-	-- Stimulus signals - signals mapped to the input and inout ports of tested entity
 	signal clk : std_logic;
 	signal reset : std_logic;
-	-- Observed signals - signals mapped to the output ports of tested entity
-
-	-- Add your code here ...
 
 begin
 
